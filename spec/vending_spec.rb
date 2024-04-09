abd48d60e92f3dc78609fca3a3a8bdbfee2b4786
Fedor Lukyanov <sleekybadger@gmail.com>
2024-04-09 18:10:02 +0300

    Initial

---
+++ spec/vending_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Vending do
+  it "has a version number" do
+    expect(Vending::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
